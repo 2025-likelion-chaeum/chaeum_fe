9698ae702e64bad8df0e5bd149f90333444ff4bd
jinhyo <kjhyo0220@gmail.com>
2025-08-02 03:27:16 +0900

    ✨ Feat: pwa 기능 추가 (#1)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,45 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicons/favicon.ico',
+        'favicons/apple-touch-icon.png',
+        'favicons/favicon-96x96.png',
+        'favicons/favicon.svg',
+      ],
+      manifest: {
+        name: '채움',
+        short_name: '채움',
+        description: '채움',
+        icons: [
+          {
+            src: '/favicons/web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/favicons/web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+      },
+    }),
+  ],
   resolve: {
     alias: [
       { find: '@', replacement: '/src' },
