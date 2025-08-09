52a852542e510770d01ef6eb1ae356477eb0f5e1
mzxxzysy <jsyjj21@gmail.com>
2025-08-10 07:55:35 +0900

    ✨ Feat: 메인 페이지 컴포넌트 제작 (#13)

--- src/pages/MainPage/MainPage.styles.ts
+++ src/pages/MainPage/MainPage.styles.ts
@@ -1,4 +1,41 @@
 import styled from '@emotion/styled';
-// import palette from '@styles/theme';
+import palette from '@styles/theme';
 
-export const MainPage = styled.div``;
+export const MainPage = styled.div`
+  margin: 28px 16px;
+  width: calc(100% - 32px);
+
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
+`;
+
+export const Semibold18 = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+  color: ${palette.grayscale.base};
+`;
+
+export const Group = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const Region = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+
+  width: 100%;
+  overflow: scroll;
+`;
+
+export const ReccomandList = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+
+  width: 100%;
+  overflow: scroll;
+`;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,92 @@
 import * as M from './MainPage.styles';
+import RegionButton from './components/RegionButton/RegionButton';
+import RecommandBox from './components/RecommandBox/RecommandBox';
+import exampleImg from '@assets/ex_recHome.svg';
+
+import { useState } from 'react';
 
 const MainPage = () => {
-  return <M.MainPage>🏡 메인 페이지</M.MainPage>;
+  const [region, setRegion] = useState<string>('');
+
+  const regions: string[] = [
+    '서울',
+    '경기',
+    '인천',
+    '부산',
+    '대구',
+    '대전',
+    '광주',
+    '울산',
+    '세종',
+    '충북',
+    '충남',
+    '경북',
+    '경남',
+    '전북',
+    '전남',
+    '강원',
+    '제주',
+  ];
+
+  const recommandData = [
+    {
+      img: exampleImg,
+      type: '농가주택 매매',
+      price: '매매 9,000만원',
+      region: '충청남도 서천군',
+      size: '대 529m²(160평)',
+      tag: ['조용한', '자연의'],
+    },
+    {
+      img: exampleImg,
+      type: '한옥 임대',
+      price: '월세 50만원',
+      region: '전라북도 전주시',
+      size: '대 120m²(36평)',
+      tag: ['전통적인', '한적한', '문화'],
+    },
+    {
+      img: exampleImg,
+      type: '상가 매매',
+      price: '매매 2억 5,000만원',
+      region: '경기도 고양시',
+      size: '대 200m²(60평)',
+      tag: ['상권좋은', '교통편리'],
+    },
+  ];
+
+  return (
+    <M.MainPage>
+      <M.Group>
+        <M.Semibold18>어느 지역을 찾으시나요?</M.Semibold18>
+        <M.Region>
+          {regions.map((region) => (
+            <RegionButton key={region} text={region} onClick={() => setRegion(region)} />
+          ))}
+        </M.Region>
+      </M.Group>
+      <M.Group>
+        <div>
+          <M.Semibold18 style={{ marginBottom: '3px' }}>멋사 님이 관심있는</M.Semibold18>
+          <M.Semibold18>창업 공간을 위한 빈집들이에요</M.Semibold18>
+        </div>
+        <M.ReccomandList>
+          {recommandData.map((item, idx) => (
+            <RecommandBox
+              key={idx}
+              img={item.img}
+              type={item.type}
+              price={item.price}
+              region={item.region}
+              size={item.size}
+              tag={item.tag}
+              onClick={() => console.log(`${item.type} 클릭됨`)}
+            />
+          ))}
+        </M.ReccomandList>
+      </M.Group>
+    </M.MainPage>
+  );
 };
 
 export default MainPage;
