556562440d657428025ba4f929012859638f8dc6
jinhyo <kjhyo0220@gmail.com>
2025-08-17 15:40:42 +0900

    ✨ Feat: 지도 띄우기 완성 (#12)

--- src/pages/MapPage/MapPage.styles.ts
+++ src/pages/MapPage/MapPage.styles.ts
@@ -1,4 +1,9 @@
 import styled from '@emotion/styled';
 // import palette from '@styles/theme';
 
-export const MapPage = styled.div``;
+export const MapPage = styled.div`
+  width: 100%;
+  height: 100dvh;
+
+  position: relative;
+`;
--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -1,7 +1,40 @@
+import { useEffect } from 'react';
 import * as M from './MapPage.styles';
 
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    kakao: any;
+  }
+}
+
 const MapPage = () => {
-  return <M.MapPage>🏡 지도 페이지</M.MapPage>;
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_KAKAO_KEY}&autoload=false`;
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById('map');
+        if (!container) return;
+
+        const options = {
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+          level: 3,
+        };
+
+        new window.kakao.maps.Map(container, options);
+      });
+    };
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  return <M.MapPage id="map"></M.MapPage>;
 };
 
 export default MapPage;
