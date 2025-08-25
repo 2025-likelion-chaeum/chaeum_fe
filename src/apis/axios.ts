a4db6bec4ebbaa89ef2429fde9ef2099789afbf4
jinhyo <kjhyo0220@gmail.com>
2025-08-25 20:32:18 +0900

    ✨ Feat: 쿠키 없는 경우 로그인 페이지로 리다이렉션 (#32)

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -4,3 +4,14 @@ export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL,
   withCredentials: true,
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.log(error);
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  },
+);
