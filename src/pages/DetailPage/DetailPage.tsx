75c2c9d6b4e75b0285e2a7753866694b9dda054b
mzxxzysy <jsyjj21@gmail.com>
2025-09-20 16:30:36 +0900

    ✨ Feat: 디테일 페이지 스크랩 여부 확인 (#53)

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -49,6 +49,7 @@ const DetailPage = () => {
           saleType: mappedSaleType,
           dealType: mappedDealType,
         });
+        setBookMarkClicked(data.scrapped);
         console.log(res.data);
       } catch (err) {
         console.error('디테일 불러오기 실패', err);
@@ -60,11 +61,12 @@ const DetailPage = () => {
   const handleScrap = async () => {
     try {
       if (bookMarkClicked) {
-        await scrap(Number(id));
-      } else {
         await deleteScrap(Number(id));
+        setBookMarkClicked(false);
+      } else {
+        await scrap(Number(id));
+        setBookMarkClicked(true);
       }
-      setBookMarkClicked((prev) => !prev);
     } catch {
       alert('스크랩 실패');
     }
