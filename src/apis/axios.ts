a1e0341211c664be987ff5e87a9af7456e5bc904
jinhyo <kjhyo0220@gmail.com>
2025-09-26 04:23:03 +0900

    ✨ Feat: 응답 interceptors 설정 (#60)

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -22,3 +22,16 @@ axiosInstance.interceptors.request.use(
 
   (error) => Promise.reject(error),
 );
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response && error.response.status === 401) {
+      window.localStorage.removeItem(LOCAL_STORAGE_KEY.token);
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  },
+);
