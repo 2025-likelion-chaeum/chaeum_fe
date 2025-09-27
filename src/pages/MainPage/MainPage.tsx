495e07c4f9f3e803a2dcffb5a3a6ce92dd2135e0
mzxxzysy <jsyjj21@gmail.com>
2025-09-27 11:39:45 +0900

    🐛 Fix: 방금 등록한 집 리스트 순서 수정 (#56)

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -114,19 +114,17 @@ const MainPage = () => {
           </M.Regular12>
         </M.Title>
         <M.HomeList>
-          {HomeData.slice()
-            .reverse()
-            .map((item) => (
-              <HomeItem
-                key={item.id}
-                id={item.id}
-                img={item.imageUrls[0]}
-                type={SALE_TYPE_REVERSE_MAP[item.saleType] || item.saleType}
-                title={item.title || '미정'}
-                region={item.address}
-                size={item.area || '불확실'}
-              />
-            ))}
+          {HomeData.slice().map((item) => (
+            <HomeItem
+              key={item.id}
+              id={item.id}
+              img={item.imageUrls[0]}
+              type={SALE_TYPE_REVERSE_MAP[item.saleType] || item.saleType}
+              title={item.title || '미정'}
+              region={item.address}
+              size={item.area || '불확실'}
+            />
+          ))}
         </M.HomeList>
       </M.Group>
     </M.MainPage>
